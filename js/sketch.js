aaf8018a6e3bb41a8889e7f13b61cff1bc739d2d
Andr34G <arellanoga4@gmail.com>
2025-03-18 16:54:40 -0700

    p5j

---
+++ js/sketch.js
@@ -0,0 +1,47 @@
+
+let canvas;
+let xPos = 0;
+let yPos = 0;
+let easing = .001;
+
+function setup (){
+    canvas=createCanvas(windowWidth, windowHeight);
+    canvas.position(0, 0)
+    canvas.style("z-index", -2);
+    //background(225);
+}
+
+
+function windowResized(){
+   resizeCanvas(windowWidth, windowHeight);
+}
+
+let img1;
+
+
+function preload () {
+  img1 = loadImage('/images/MiffyPNG.png');
+  
+}
+
+
+function draw() {
+  
+  
+  
+}
+
+function draw(){
+   clear ();
+   drawThing(mouseX, mouseY)
+}
+
+function mouseMoved(){
+    drawThing()
+}
+
+function drawThing(){
+    //background(220);
+  noTint();
+    image(img1, mouseX, mouseY,50, 50);  
+}
